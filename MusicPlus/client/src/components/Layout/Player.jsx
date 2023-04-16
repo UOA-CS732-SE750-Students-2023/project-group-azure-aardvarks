25d0b4a09449588abd3501e7503b84437134bae2
dxh000130 <34475380+dxh000130@users.noreply.github.com>
2023-04-17 10:03:40 +1200

    Fix bug

--- MusicPlus/client/src/components/Layout/Player.jsx
+++ MusicPlus/client/src/components/Layout/Player.jsx
@@ -3,37 +3,71 @@ import "react-jinke-music-player/assets/index.css";
 import b from '../../static/b.mp3'
 import {useState} from "react";
 
+let playlistId;
+
+async function newPlaylist() {
+    try {
+        const response = await fetch("http://127.0.0.1:3000/api/playList/newPlayList", {
+            method: 'POST', // 指定请求方法为POST
+            headers: {
+                'Content-Type': 'application/json', // 设置请求头，指定数据类型为JSON
+                'Authorization': 'Basic ' + btoa('dxh000130' + ":" + 'duan002349')
+            },
+            body: JSON.stringify({
+                "name": "test",
+                "private" : false,
+                "songs" : ["1974443814","1974443815","1974443816","1974443817"]
+
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const responseData = await response.json(); // 假设服务器返回的是JSON格式数据
+        playlistId = responseData.data._id
+        return responseData;
+    } catch (error) {
+        console.error('Error posting data:', error);
+    }
+}
 
 function LoadPlaylistButton({ setAudioLists }) {
     const getPlayList = async () => {
-        const songSearchResponse = await fetch(
-            "http://127.0.0.1:3000/api/playList/searchPlayListById/6436761b8ef88a8a1f6828b5"
-        );
-        let songSearchData = await songSearchResponse.json();
-
-        console.log("Search", songSearchData);
-        songSearchData = songSearchData.data[0].songs;
-
-        for (let song in songSearchData) {
-            const lyricResponse = await fetch(
-                "http://127.0.0.1:3000/api/music/lyric/" + song
+        try{
+            const songSearchResponse = await fetch(
+                "http://127.0.0.1:3000/api/playList/searchPlayListById/"+playlistId
             );
-            let lyricData = await lyricResponse.json();
-            const songFile = await fetch("http://127.0.0.1:3000/api/music/play/" + song)
-            let songData = await songFile.blob()
-            songData=URL.createObjectURL(songData);
-            console.log(songData)
-            setAudioLists((prevAudioLists) => [
-                ...prevAudioLists,
-                {
-                    _id: song,
-                    name: songSearchData[song].name,
-                    singer: songSearchData[song].singer[0].name,
-                    cover: songSearchData[song].picUrl,
-                    musicSrc: songData,
-                    lyric: lyricData.data,
-                },
-            ]);
+            let songSearchData = await songSearchResponse.json();
+
+            console.log("Search", songSearchData);
+            songSearchData = songSearchData.data[0].songs;
+
+            for (let song in songSearchData) {
+                const lyricResponse = await fetch(
+                    "http://127.0.0.1:3000/api/music/lyric/" + song
+                );
+                let lyricData = await lyricResponse.json();
+                const songFile = await fetch("http://127.0.0.1:3000/api/music/play/" + song)
+                let songData = await songFile.blob()
+                songData=URL.createObjectURL(songData);
+                console.log(songData)
+                setAudioLists((prevAudioLists) => [
+                    ...prevAudioLists,
+                    {
+                        _id: song,
+                        name: songSearchData[song].name,
+                        singer: songSearchData[song].singer[0].name,
+                        cover: songSearchData[song].picUrl,
+                        musicSrc: songData,
+                        lyric: lyricData.data,
+                    },
+                ]);
+            }
+        }
+        catch (e) {
+            console.log(e)
         }
     };
 
@@ -41,6 +75,7 @@ function LoadPlaylistButton({ setAudioLists }) {
 }
 
 function Player() {
+    newPlaylist();
     const [audioLists, setAudioLists] = useState([{
         name: "b",
         singer: "b",
