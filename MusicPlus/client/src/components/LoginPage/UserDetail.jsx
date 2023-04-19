6b9400a7bf5578626a2f77e14bc90ec0005a5b8e
dxh000130 <34475380+dxh000130@users.noreply.github.com>
2023-04-19 16:00:14 +1200

    显示头像

---
+++ MusicPlus/client/src/components/LoginPage/UserDetail.jsx
@@ -0,0 +1,18 @@
+import Layout from "../Layout/Layout.jsx";
+import ActionSlide from "../HomePage/ActionSlide.jsx";
+import {AlbumCover} from "../Album/AlbumCover.jsx";
+import {useState} from "react";
+
+
+function UserDetail(){
+
+    return(
+        <>
+            <Layout>
+                <h2>User Detail</h2>
+
+            </Layout>
+        </>
+    )
+}
+export default UserDetail
