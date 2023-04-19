26e2f48d7029b9f9008f83f6329051c81bd4ac8c
dxh000130 <34475380+dxh000130@users.noreply.github.com>
2023-04-19 17:57:05 +1200

    Update UserDetail.jsx

--- MusicPlus/client/src/components/LoginPage/UserDetail.jsx
+++ MusicPlus/client/src/components/LoginPage/UserDetail.jsx
@@ -25,7 +25,7 @@ function UserDetail(){
                         <Col md={12}>
                             {/* Basic Information */}
                             <Card className="mb-4">
-                                <Card.Header>基础信息</Card.Header>
+                                <Card.Header>Basic information</Card.Header>
                                 <Card.Body>
                                     <Form>
                                         <Form.Group>
@@ -37,87 +37,87 @@ function UserDetail(){
                                             <Form.Control type="file" />
                                         </Form.Group>
                                         <Form.Group>
-                                            <Form.Label>生日</Form.Label>
+                                            <Form.Label>DOB</Form.Label>
                                             <Form.Control type="date" />
                                         </Form.Group>
                                         <Form.Group>
-                                            <Form.Label>性别</Form.Label>
+                                            <Form.Label>Gender</Form.Label>
                                             <Form.Check
                                                 type="radio"
-                                                label="男"
+                                                label="M"
                                                 name="gender"
                                                 id="male"
                                             />
                                             <Form.Check
                                                 type="radio"
-                                                label="女"
+                                                label="F"
                                                 name="gender"
                                                 id="female"
                                             />
                                         </Form.Group>
-                                        <Button type="submit">保存</Button>
+                                        <Button type="submit">Save</Button>
                                     </Form>
                                 </Card.Body>
                             </Card>
 
                             {/* Contact Information */}
                             <Card className="mb-4">
-                                <Card.Header>联系方式</Card.Header>
+                                <Card.Header>Contact</Card.Header>
                                 <Card.Body>
                                     <Form>
                                         <Form.Group>
-                                            <Form.Label>邮箱</Form.Label>
+                                            <Form.Label>Email</Form.Label>
                                             <Form.Control type="email" placeholder="example@example.com" />
                                         </Form.Group>
                                         <Form.Group>
-                                            <Form.Label>手机号</Form.Label>
+                                            <Form.Label>Phone</Form.Label>
                                             <Form.Control type="tel" placeholder="123-456-7890" />
                                         </Form.Group>
-                                        <Button type="submit">保存</Button>
+                                        <Button type="submit">Save</Button>
                                     </Form>
                                 </Card.Body>
                             </Card>
 
                             {/* Address */}
                             <Card className="mb-4">
-                                <Card.Header>地址</Card.Header>
+                                <Card.Header>Address</Card.Header>
                                 <Card.Body>
                                     <Form>
                                         <Form.Group>
-                                            <Form.Label>街道</Form.Label>
+                                            <Form.Label>Street</Form.Label>
                                             <Form.Control type="text" placeholder="1234 Main St" />
                                         </Form.Group>
                                         <Form.Group>
-                                            <Form.Label>城市</Form.Label>
+                                            <Form.Label>City</Form.Label>
                                             <Form.Control type="text" placeholder="City" />
                                         </Form.Group>
                                         <Form.Group>
-                                            <Form.Label>邮政编码</Form.Label>
+                                            <Form.Label>Postcode</Form.Label>
                                             <Form.Control type="text" placeholder="ZIP" />
                                         </Form.Group>
-                                        <Button type="submit">保存</Button>
+                                        <Button type="submit">Save</Button>
                                     </Form>
                                 </Card.Body>
                             </Card>
 
                             {/* Password */}
                             <Card className="mb-4">
-                                <Card.Header>密码</Card.Header>
+                                <Card.Header>Password</Card.Header>
                                 <Card.Body>
                                     <Form>
                                         <Form.Group>
-                                            <Form.Label>当前密码</Form.Label>
+                                            <Form.Label>Current Password</Form.Label>
                                             <Form.Control type="password" />
                                         </Form.Group>
                                         <Form.Group>
-                                            <Form.Label>新密码</Form.Label>
+                                            <Form.Label>New password</Form.Label>
                                             <Form.Control type="password" />
                                         </Form.Group>
                                         <Form.Group>
-                                            <Form.Label>确认新密码</Form.Label>
+                                            <Form.Label>double check password</Form.Label>
                                             <Form.Control type="password" />
                                         </Form.Group>
-                                        <Button type="submit">修改密码</Button>
+                                        <Button type="submit">change password</Button>
                                     </Form>
                                 </Card.Body>
                             </Card>
