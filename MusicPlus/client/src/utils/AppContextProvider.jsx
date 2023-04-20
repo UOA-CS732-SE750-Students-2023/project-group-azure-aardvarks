48d98abcedbeed1d45fb34b52bbe4b9db7333411
dxh000130 <34475380+dxh000130@users.noreply.github.com>
2023-04-20 13:27:25 +1200

    1

--- MusicPlus/client/src/utils/AppContextProvider.jsx
+++ MusicPlus/client/src/utils/AppContextProvider.jsx
@@ -1,13 +1,20 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useContext} from "react";
 import Cookies from 'js-cookie';
 import b from "../static/b.mp3";
+import {useNavigate} from "react-router-dom";
 import Player from "../components/Layout/Player.jsx";
 import axios from "axios";
+import PlayListNotification from "../components/Notification/GlobalNotification.jsx";
+import TemporaryPlayList from "../components/Playlist/TemporaryPlayList.jsx";
+import {audioStreamToBlob, fetchSongDetailsAsync, formatDateTime, formatSinger} from "./commentUtils.js";
+import {BACKEND_API} from "./env.js";
 const backendAPI = import.meta.env.VITE_BACKEND_BASE_URL;
 // Manage the user's status
 export const UserContext = React.createContext({})
-
-
+// Sending a notification
+export const NotificationContext = React.createContext([])
+// Manage the temporary Playlist
+export const TemporaryPlayListContext = React.createContext([])
 const PlayerContext = React.createContext({
     // showPlayer: false,
     // setShowPlayer: () => {},
@@ -15,13 +22,16 @@ const PlayerContext = React.createContext({
 
 export default PlayerContext;
 
+
 /**
  * Put it to the Root component. Global user status management.
+ * Load the information of current user
  * @param children  Root entry
  * @returns {JSX.Element}
  */
 export function UserProvider({ children }) {
     const [userDetail, setUserDetail] = useState({});
+    const [userPlaylist, setUserPlaylist] = useState([]);
     const expiresDate = new Date();
     expiresDate.setDate(expiresDate.getDate() + 30);
     const setCookieUserDetail = (detail) => {
@@ -32,7 +42,7 @@ export function UserProvider({ children }) {
     useEffect(() => {
         const fetchUserDetail = async () => {
             const cookieUserDetail = Cookies.get('userDetail');
-
+            console.log(cookieUserDetail)
             if (cookieUserDetail) {
                 const parsedUserDetail = JSON.parse(cookieUserDetail);
                 const detail = await axios.get(`${backendAPI}/api/user/logIn`, {
@@ -48,9 +58,23 @@ export function UserProvider({ children }) {
         fetchUserDetail();
     }, []);
 
+    useEffect( () => {
+        const getMyPlayList = async ()=>{
+            if (userDetail._id === undefined){
+                setUserPlaylist([])
+            }else {
+                const myPlayList = await axios.get(`${BACKEND_API}/api/playList/searchPlayListByOwnerId/${userDetail._id}`)
+                setUserPlaylist(myPlayList.data.data)
+            }
+        }
+        getMyPlayList()
+    },[userDetail]) // Once user updated, the render the playlist again
+
     const context = {
         userDetail,
         setUserDetail: setCookieUserDetail,
+        userPlaylist,
+        setUserPlaylist
     };
 
     return (
@@ -83,5 +107,88 @@ export function PlayerProvider({children}){
             {children}
             {showPlayer && <Player />}
         </PlayerContext.Provider>
+    )
+}
+
+
+export function NotificationProvider({children}){
+    const [show, setShow] = useState(true);
+    const [notification, setNotification] = useState({
+        show:true,
+        header:"",
+        time:"",
+        content:""
+    })
+    const deleteNotification = () =>{
+        console.log(1)
+    }
+    const context = {
+        show, setShow,deleteNotification
+    }
+    return (
+        <NotificationContext.Provider value={context}>
+            {children}
+            {<PlayListNotification/>}
+        </NotificationContext.Provider>
+    )
+}
+
+
+export function TemporaryPlaylistProvider({children}){
+    const [show, setShow] = useState(false);
+    const [songList, setSongList] = useState([]);
+    const [lastUpdate, setLastUpdate] = useState(formatDateTime(new Date()));
+    const [temporaryPlaylistDetail, setTemporaryPlaylistDetail] = useState('');
+    const {currentPlayList, setCurrentPlayList} = useContext(PlayerContext);
+    const removeTemporaryPlaylist = () =>{
+        setShow(false)
+        setSongList([])
+        setTemporaryPlaylistDetail([])
+    }
+    const addToCurrentPlaylist = async () =>{
+
+        for (const i in songList) {
+            const request = await fetchSongDetailsAsync(songList[i]._id)
+            const lyric = request.lyric
+            const audioStream = request.audioStream
+
+            const musicSrc = audioStreamToBlob(audioStream)
+            const musicDetail = {
+                _id:songList[i]._id,
+                name:songList[i].name,
+                cover:songList[i].album.picUrl,
+                musicSrc:musicSrc,
+                lyric:lyric.data.data,
+                singer:formatSinger(songList[i].singer)
+            }
+            // React 函数式更新(Functional updates)， 不会改变之前的状态，而是通过创建新的数组实现状态更新，这有助于避免副作用带来的各种问题
+            setCurrentPlayList(prevList => [...prevList, musicDetail])
+            // setCurrentPlayList([...currentPlayList, musicDetail])
+        }
+    }
+    const showTemporaryPlaylist = () =>{
+        setShow(true)
+    }
+    const addToTemporaryPlaylist = (song)=>{
+        setSongList([...songList, song])
+        setTemporaryPlaylistDetail(`${temporaryPlaylistDetail}  ${song.name}`)
+        setLastUpdate(formatDateTime(new Date()))
+        showTemporaryPlaylist()
+    }
+
+    const context = {
+        removeTemporaryPlaylist,
+        addToCurrentPlaylist,
+        showTemporaryPlaylist,
+        addToTemporaryPlaylist,
+        show,
+        lastUpdate,
+        temporaryPlaylistDetail
+    }
+    return (
+        <TemporaryPlayListContext.Provider value={context}>
+            {children}
+            {<TemporaryPlayList/>}
+        </TemporaryPlayListContext.Provider>
     )
 }
