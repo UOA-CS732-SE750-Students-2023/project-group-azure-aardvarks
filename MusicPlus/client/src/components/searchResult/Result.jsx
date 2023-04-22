63e9ec957bcaeaaf022ff607921c9c67ea76f3dd
yihangalan <88365262+yihangalan@users.noreply.github.com>
2023-04-22 21:05:56 +1200

    add button for search

--- MusicPlus/client/src/components/searchResult/Result.jsx
+++ MusicPlus/client/src/components/searchResult/Result.jsx
@@ -1,27 +1,59 @@
 import Layout from "../Layout/Layout.jsx";
-import React, { useEffect, useState } from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import axios from "axios";
 import {BACKEND_API} from "../../utils/env.js";
-import {useToast} from "../../utils/AppContextProvider.jsx";
-import { useLocation } from "react-router-dom";
-import {ListGroup, Row} from "react-bootstrap";
+import PlayerContext, {useToast} from "../../utils/AppContextProvider.jsx";
+import {Link, useLocation} from "react-router-dom";
+import {Col, ListGroup, Row} from "react-bootstrap";
+import "./index.css"
 
 export default function Result() {
+    const {setShowPlayer} = useContext(PlayerContext)
     const [searchTerm, setSearchTerm] = useState('');
-    const [result, setResult] = useState(null)
-    const { addToast } = useToast();
     const [isLoading, setIsLoading] = useState(true);
+
+
+    const [songSearch, setSongSearch] = useState(true);
+    const [singerSearch, setSingerSearch] = useState(false);
+    const [albumSearch, setAlbumSearch] = useState(false);
+
+    const [result, setResult] = useState(null)
+    const [singer, setSinger] = useState(null)
+    const [album, setAlbum] = useState(null)
+
+    function clickSong(){
+        setSongSearch(true)
+        setSingerSearch(false)
+        setAlbumSearch(false)
+    }
+
+    function clickSinger(){
+        setSongSearch(false)
+        setSingerSearch(true)
+        setAlbumSearch(false)
+    }
+
+    function clickAlbum(){
+        setSongSearch(false)
+        setSingerSearch(false)
+        setAlbumSearch(true)
+    }
+
+
+
+    const { addToast } = useToast();
     const location = useLocation();
 
     useEffect(() => {
         const extractedSearchTerm = extractSearchTermFromUrl(location);
         setSearchTerm(extractedSearchTerm);
+        setShowPlayer(true);
 
         const getResult = async () => {
             try {
                 await axios.get(`${BACKEND_API}/api/search/search/${extractedSearchTerm}/${0}/${10}`).then(response => {
                     setResult(response.data.data.song)
-                    console.log(response.data.data.song)
+                    console.log(response.data.data)
                     setIsLoading(false);
                 });
             } catch (error) {
@@ -36,8 +68,25 @@ export default function Result() {
 
     return (
         <Layout>
-            <div>
-                <h1>Search Page</h1>
+            <Row className="mb-4">
+                <Col>
+                    <div className="wrap mt-4">
+                        <button className="button_search" onClick={()=>clickSong()}>song</button>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="wrap mt-4">
+                        <button className="button_search" onClick={()=>clickSinger()}>singer</button>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="wrap mt-4">
+                        <button className="button_search" onClick={()=>clickAlbum()}>album</button>
+                    </div>
+                </Col>
+            </Row>
+
+            <div style={{display: songSearch?"block":"none"}}>
                 {isLoading ? (
                     <p>Loading...</p>
                 ) : (
@@ -47,8 +96,15 @@ export default function Result() {
                     )}
                 </ListGroup>
                 )}
+            </div>
 
+            <div style={{display: singerSearch?"block":"none"}}>
+                <h1>singer</h1>
 
+            </div>
+
+            <div style={{display: albumSearch?"block":"none"}}>
+                <h1>album</h1>
             </div>
         </Layout>
     );
