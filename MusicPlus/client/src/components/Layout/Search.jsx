1c0ea83c3dc5ef9cf32a26a323ffcf9db888fd9c
yihangalan <88365262+yihangalan@users.noreply.github.com>
2023-04-22 17:48:26 +1200

    first fetch search result

--- MusicPlus/client/src/components/Layout/Search.jsx
+++ MusicPlus/client/src/components/Layout/Search.jsx
@@ -19,7 +19,6 @@ export default function Search(){
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
-        console.log(inputValue);
     };
 
     return(
--- MusicPlus/client/src/components/searchResult/Result.jsx
+++ MusicPlus/client/src/components/searchResult/Result.jsx
@@ -1,28 +1,60 @@
 import Layout from "../Layout/Layout.jsx";
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import {BACKEND_API} from "../../utils/env.js";
+import {useToast} from "../../utils/AppContextProvider.jsx";
+import { useLocation } from "react-router-dom";
+import {ListGroup, Row} from "react-bootstrap";
 
 export default function Result() {
-    const [params, setParams] = useState({});
+    const [searchTerm, setSearchTerm] = useState('');
+    const [result, setResult] = useState(null)
+    const { addToast } = useToast();
+    const [isLoading, setIsLoading] = useState(true);
+    const location = useLocation();
 
     useEffect(() => {
-        setParams(extractParamsFromUrl());
-    }, [params]);
+        const extractedSearchTerm = extractSearchTermFromUrl(location);
+        setSearchTerm(extractedSearchTerm);
+
+        const getResult = async () => {
+            try {
+                await axios.get(`${BACKEND_API}/api/search/search/${extractedSearchTerm}/${0}/${10}`).then(response => {
+                    setResult(response.data.data.song)
+                    console.log(response.data.data.song)
+                    setIsLoading(false);
+                });
+            } catch (error) {
+                console.log(error);
+                addToast("search music error! Please contect us! We will fix it ASAP!")
+            }
+        };
+        getResult()
+
+    }, [location]);
+
 
     return (
         <Layout>
             <div>
                 <h1>Search Page</h1>
-                <p>Search Term: {params.searchTerm}</p>
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <ListGroup>
+                    {result.map((song) =>
+                        <ListGroup.Item key={song.id}> {song.name} </ListGroup.Item>
+                    )}
+                </ListGroup>
+                )}
+
+
             </div>
         </Layout>
     );
 }
 
-function extractParamsFromUrl() {
-    const hash = window.location.hash;
-    const searchTerm = hash.split('/').pop();
-
-    return {
-        searchTerm: decodeURIComponent(searchTerm),
-    };
+function extractSearchTermFromUrl(location) {
+    const hash = location.pathname;
+    return decodeURIComponent(hash.split('/').pop());
 }
