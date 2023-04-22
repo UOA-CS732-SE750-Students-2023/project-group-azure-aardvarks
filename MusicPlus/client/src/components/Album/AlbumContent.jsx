fea7879c7d37b0d76b901949ac502480a09d4832
yihangalan <88365262+yihangalan@users.noreply.github.com>
2023-04-23 01:21:33 +1200

    fix bug

--- MusicPlus/client/src/components/Album/AlbumContent.jsx
+++ MusicPlus/client/src/components/Album/AlbumContent.jsx
@@ -1,20 +1,20 @@
 import Layout from "../Layout/Layout.jsx";
-import {Link, useLocation} from "react-router-dom";
+import {useLocation} from "react-router-dom";
 import {useContext, useEffect, useState} from "react";
 import axios from "axios";
 import {BACKEND_API} from "../../utils/env.js";
 import PlayerContext, {useToast} from "../../utils/AppContextProvider.jsx";
 import {Col, Row, ListGroup} from "react-bootstrap";
 import "./index.css"
-import {map} from "react-bootstrap/ElementChildren";
+
 
 export default function AlbumContent(){
     const {setShowPlayer} = useContext(PlayerContext)
     const { addToast } = useToast();
     const location = useLocation();
 
-    const [album, setAlbum] = useState();
-    const [song, setSong] = useState();
+    const [album, setAlbum] = useState(null);
+    const [song, setSong] = useState(null);
     const [load, setLoad] = useState(false);
 
 
@@ -23,40 +23,40 @@ export default function AlbumContent(){
         console.log(extractedSearchTerm)
         setShowPlayer(true);
         const getResult = async () => {
-            setLoad(true)
             try {
+                setLoad(true)
                 await axios.get(`${BACKEND_API}/api/album/${extractedSearchTerm}`).then(response => {
                     setAlbum(response.data.data.album);
                     setSong(response.data.data.songs);
-                    console.log(response.data.data)
+                    console.log(response.data.data.album)
+                    setLoad(false)
                 });
 
             } catch (error) {
                 console.log(error);
                 addToast("search album error! Please contact us! We will fix it ASAP!")
             }
-            setLoad(false)
+
         };
         getResult();
 
     }, [location]);
+
     return(
         <Layout>
             {load?<p>loading...</p>:
                 <div>
                     <Row>
-                        <img className="imgSize" src={album.picUrl} alt="My Image" />
+                        <img className="imgSize" src={album?album.picUrl:"a"} alt="My Image" />
                         <Col>
-                            <Row><h1>{album.name}</h1></Row>
-                            <Row className="mt-4"><p>singer: {album.artist.name}</p></Row>
+                            <Row><h1>{album?album.name:"a"}</h1></Row>
+                            <Row className="mt-4"><p>singer: {album?album.name:"a"}</p></Row>
                         </Col>
                     </Row>
                     <Row className="mt-4">
                         <ListGroup>
-                            {song.map((s) => <ListGroup.Item>{s.name}</ListGroup.Item>)}
-
+                            {song?song.map((s) => <ListGroup.Item key={s.id}>{s?s.name:"a"}</ListGroup.Item>):"test"}
                         </ListGroup>
-
                     </Row>
                 </div>
             }
