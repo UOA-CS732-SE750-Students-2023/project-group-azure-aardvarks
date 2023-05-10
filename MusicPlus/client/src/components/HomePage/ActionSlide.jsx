46ace3cc4e8fc04e736bda11130412f3b592df10
weilin <wche667@aucklanduni.ac.nz>
2023-05-10 13:07:44 +1200

    home album cover

--- MusicPlus/client/src/components/HomePage/ActionSlide.jsx
+++ MusicPlus/client/src/components/HomePage/ActionSlide.jsx
@@ -3,28 +3,35 @@ import Carousel from 'react-bootstrap/Carousel';
 import downloadImage from '../../../public/download.jpg';
 import code_structure from '../../../public/code_structure.png';
 import Layout from "../Layout/Layout.jsx";
+import { BACKEND_API } from "../../utils/env.js";
+import { useState, useEffect} from "react";
 
-function ActionSlide(){
+function ActionSlide() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/album/list?limit=5")
+            .then(response => response.json())
+            .then(data => setProducts(data.products))
+            .catch(error => console.error(error));
+    }, []);
+
+    const coverUrls = products.map(product => product.coverUrl);
+
     return (
         <>
             <Carousel variant={"dark"}>
-                <Carousel.Item interval={1000}>
+            {coverUrls.map(coverUrl => (
+                    <Carousel.Item interval={1000}>
                     <img
-                        height={400}
-                        className="d-block w-100"
-                        src={code_structure}
+                        style={{ maxWidth: '30%', maxHeight: '30%' }}
+                        className="mx-auto d-block"
+                        src={coverUrl}
                         alt="First slide"
                     />
                 </Carousel.Item>
-                <Carousel.Item interval={1000}>
-                    <img
-                        height={400}
-                        className="d-block w-100"
-                        src={code_structure}
-                        alt="First slide"
-                    />
-                </Carousel.Item>
-
+       
+                ))}
             </Carousel>
         </>
     );
