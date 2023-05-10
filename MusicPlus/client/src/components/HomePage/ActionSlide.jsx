5e74cd21c777fc3519f3d962a629edddb19453e2
weilin <wche667@aucklanduni.ac.nz>
2023-05-10 13:33:04 +1200

    home cover

--- MusicPlus/client/src/components/HomePage/ActionSlide.jsx
+++ MusicPlus/client/src/components/HomePage/ActionSlide.jsx
@@ -4,10 +4,15 @@ import downloadImage from '../../../public/download.jpg';
 import code_structure from '../../../public/code_structure.png';
 import Layout from "../Layout/Layout.jsx";
 import { BACKEND_API } from "../../utils/env.js";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 function ActionSlide() {
     const [products, setProducts] = useState([]);
+    const navigate = useNavigate();
+    const { id } = useParams();
+    
 
     useEffect(() => {
         fetch("http://localhost:4000/album/list?limit=5")
@@ -17,20 +22,27 @@ function ActionSlide() {
     }, []);
 
     const coverUrls = products.map(product => product.coverUrl);
+    const albumIds = products.map(product => product.albumId);
+
+    const handleImageClick = (albumId) => {
+        navigate(`/album/${albumId}`);
+    }
 
     return (
         <>
             <Carousel variant={"dark"}>
-            {coverUrls.map(coverUrl => (
-                    <Carousel.Item interval={1000}>
-                    <img
-                        style={{ maxWidth: '30%', maxHeight: '30%' }}
-                        className="mx-auto d-block"
-                        src={coverUrl}
-                        alt="First slide"
-                    />
-                </Carousel.Item>
-       
+                {coverUrls.map((coverUrl, index) => (
+                    <Carousel.Item interval={2000} key={index}>
+                        <img
+                            style={{ maxWidth: '30%', maxHeight: '30%', cursor: 'pointer' }}
+                            className="mx-auto d-block"
+                            src={coverUrl}
+                            alt="First slide"
+                            onClick={() => handleImageClick(albumIds[index])}
+
+                        />
+                    </Carousel.Item>
+
                 ))}
             </Carousel>
         </>
