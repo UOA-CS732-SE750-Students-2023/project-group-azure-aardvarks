b586761de2b2cd804c0a72ddfc61a555dcdef21d
weilin <wche667@aucklanduni.ac.nz>
2023-05-11 11:31:52 +1200

    delete chongqian

--- MusicPlus/client/src/components/Layout/LayoutHeader.jsx
+++ MusicPlus/client/src/components/Layout/LayoutHeader.jsx
@@ -23,18 +23,7 @@ function LayoutHeader() {
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="./#/home">Home</Nav.Link>
-                        <Nav.Link href="#pricing">充钱</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        
                     </Nav>
 
 
